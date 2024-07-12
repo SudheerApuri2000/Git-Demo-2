1df792ca5d7e62d861eade2430f78f9ce4fe1657
Sudheer <sudheer9017@gmail.com>
2024-07-12 09:04:37 +0530

    Python codes

---
+++ First.py
@@ -0,0 +1,47 @@
+#-------------Pos or neg---------------
+
+# num = -50
+# if num >= 0:
+#     print("Positive")
+# else:
+#     print("negative")
+
+#------Even or Odd------
+
+# num = int(input("Enter number :"))
+# for i in range(1,num+1):
+#     if i%2 == 0:
+#         print(i, " Even")
+#     else:
+#         print(i," Odd")
+
+#------sum of natural no-------
+
+#n = int(input("Enter number : "))
+# sum = 0
+# for i in range (n+1):
+#     sum=sum+i
+#     print(sum)
+# print(n*(n+1)/2)
+
+#----leap year----
+
+# n = int(input("Enter year : "))
+# if (n%400==0) and (n%4==0 or n%100!=0):
+#     print("Leap year")
+# else:
+#     print("Not a leap year")
+
+#----prime----
+
+# n = int(input("Enter number : "))
+# for i in range(1,n):
+#     c=0
+#     for j in range(1,i+1):
+#         if i%j==0:
+#             c+=1
+#     if c==2:
+#         print(i," Prime")
+#     else:
+#         print(i," composite")
+
---
+++ Second.py
@@ -0,0 +1,21 @@
+# n = int(input("Enter num : "))
+# c=len(str(n))
+# t=n
+# sum=0
+# while t>0:
+#         r=t%10
+#         sum=sum+r**c
+#         t//=10
+        
+# if sum==n:
+#         print("Armstrong")
+# else:
+#         print("not")
+
+#---Factorial---
+
+# n = int(input("Enter number : "))
+# fact = 1
+# for i in range(1,n+1):
+#     fact = fact * i
+#     print(fact)
